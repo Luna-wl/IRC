0c83dff960f9e02e0249e9fcf92f45f2b911f8da
Luna-wl <Warintorn_L@outlook.com>
2024-01-22 00:22:32 +1100

    Update Help and Info commands

--- srcs/command/Help.cpp
+++ srcs/command/Help.cpp
@@ -6,7 +6,7 @@
 /*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 01:35:25 by wluedara          #+#    #+#             */
-/*   Updated: 2024/01/21 22:56:36 by wluedara         ###   ########.fr       */
+/*   Updated: 2024/01/22 00:19:45 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,36 @@ Help::~Help() {}
 
 void Help::execute(Client * client, std::vector<std::string> &args)
 {
-	int num = args.size();
-	if (num == 2) {
-		// helpCmd(client, args[1]);
-	}
-	else if (num == 1) {
-		client->receive_message("If you not register, you can use these commands:");
-		client->receive_message("NICK <nickname>: set your nickname");
-		client->receive_message("USER <username> 0 * <realname>: set your username");
+	if (args.size() == 1) {
+		std::string message = "\nIf you not register, you can use these commands:\n";
+		message += "> NICK <nickname>: set your nickname\n";
+		message += "> USER <username> 0 * <realname>: set your username\n\n";
 		
-		client->receive_message("If you register, you can use these commands:");
-		client->receive_message("JOIN <channel>: join a channel");
-		client->receive_message("PART <channel>: leave a channel");
-		client->receive_message("PRIVMSG <receiver> <message>: send a message to a receiver");
-		client->receive_message("NAMES <channel>: list all users in a channel");
-		client->receive_message("LIST <channel>: list all channels");
-		client->receive_message("TOPIC <channel> <topic>: set a topic for a channel");
-		client->receive_message("QUIT: disconnect from the server");
+		message += "If you register, you can use these commands:\n";
+		message += "> INFO: show server information\n";
+		message += "> INVITE <nickname> <channel>: invite a user to a channel\n";
+		message += "> JOIN <channel>: join a channel\n";
+		message += "> KICK <channel> <nickname>: kick a user from a channel\n";
+		message += "> LIST <channel>: list all channels\n";
+		message += "> MODE <channel> <mode>: set a mode for a channel\n";
+		message += "  -i: Set/remove Invite-only channel\n";
+		message += "  -t: Set/remove the restrictions of the TOPIC command to channel operators\n";
+		message += "  -k: Set/remove the channel key (password\n";
+		message += "  -o: Give/take channel operator privilege\n";
+		message += "  -l: Set/remove the user limit to channel\n";
+		message += "> NAMES <channel>: list all users in a channel\n";
+		message += "> NOTICE <nickname> <message>: send a notice to a user\n";
+		message += "> OPER <name> <password>: to obtain IRC operator privileges\n";
+		message += "> PART <channel>: leave a channel\n";
+		message += "> PASS <password>: set your password\n";
+		message += "> PRIVMSG <receiver> <message>: send a message to a receiver\n";
+		message += "> PING <server>: ping a server\n";
+		message += "> PONG <server>: pong a server\n\n";
+		message += "> QUIT: disconnect from the server\n";
+		message += "> TIME <server>: show the time of a server\n";
+		message += "> TOPIC <channel> <topic>: set a topic for a channel\n";
+		client->receive_message(message);
 		return ;
 	}
-	else
-		client->receive_message(ERR_TOOMANYARGUMENTS(client->getUsername(), "HELP"));
+	client->receive_message(ERR_TOOMANYARGUMENTS(_srv->getName(), "HELP"));
 }
--- srcs/command/Info.cpp
+++ srcs/command/Info.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Info.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkraikua <tkraikua@student.42.th>          +#+  +:+       +#+        */
+/*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 01:35:04 by wluedara          #+#    #+#             */
-/*   Updated: 2024/01/21 19:48:40 by tkraikua         ###   ########.fr       */
+/*   Updated: 2024/01/22 00:21:37 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ void Info::execute(Client * client, std::vector<std::string> &args) {
 		client->receive_message(ERR_TOOMANYARGUMENTS(client->getUsername(), "INFO"));
 		return ;
 	}
-	// client->receive_message(RPL_INFO(client->getUsername()));
-	// client->receive_message("===== SERVER INFO =====");
-	// client->receive_message("Server name	: " + _srv->getName());
-	// client->receive_message("Port		: " + _srv->getPort());
-	// client->receive_message("Client number	: " + std::to_string(_srv->getClientNum()));
-	// client->receive_message("Channel number	: " + std::to_string(_srv->getChannelNum()));
-	std::string message = "\n===== SERVER INFO =====\n";
+	std::string message = "\n\n ^ ^            \n";
+	message += "(O,O)           \n";
+	message += "(   )   SERVER INFO   \n";
+	message += "-\"-\"-------------------\n";
 	message += "Server name	: " + _srv->getName() + "\n";
 	message += "Port		: " + _srv->getPort() + "\n";
 	message += "Client number	: " + std::to_string(_srv->getClientNum()) + "\n";
-	message += "Channel number	: " + std::to_string(_srv->getChannelNum());
+	message += "Channel number	: " + std::to_string(_srv->getChannelNum()) + "\n";
+	message += "-----------------------\n";
 	client->receive_message(RPL_INFO(_srv->getName(), args[0], message));
 }
