7bc26e8662a4e4de5f0241160a8fbda0e5f58bf2
Luna-wl <Warintorn_L@outlook.com>
2024-01-22 01:18:37 +1100

    edit list and topic

--- srcs/command/List.cpp
+++ srcs/command/List.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   List.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkraikua <tkraikua@student.42.th>          +#+  +:+       +#+        */
+/*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 00:33:28 by wluedara          #+#    #+#             */
-/*   Updated: 2024/01/21 19:35:37 by tkraikua         ###   ########.fr       */
+/*   Updated: 2024/01/22 01:02:14 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ List::List(Server * srv) : Command(srv) {}
 List::~List() {}
 
 void List::execute(Client * client, std::vector<std::string> &args) {
-	// if ( !client->isRegist() ) {
-	// 	client->receive_message(ERR_NOTREGISTERED(_srv->getName()));
-	// 	return;
-	// }
+	if ( !client->isRegist() ) {
+		client->receive_message(ERR_NOTREGISTERED(_srv->getName(), client->getNickname()));
+		return ;
+	}
 	int num = args.size();
 	std::cout << "num = " << num << std::endl;
 	if (num == 1) {
 		// LIST ALL CHANNELS
 		if (_srv->getChannels().empty())
-			client->receive_message(RPL_LISTEND(client->getUsername()));
+			client->receive_message(RPL_LISTEND(_srv->getName()));
 		else {
 			client->receive_message("+-----LIST CHANNEL-----+");
 			std::map<std::string, Channel *>::iterator it = _srv->getChannels().begin();
@@ -68,18 +68,3 @@ void List::execute(Client * client, std::vector<std::string> &args) {
 		client->receive_message(ERR_TOOMANYARGUMENTS(_srv->getName(), "LIST"));
 	}
 }
-
-// RPL_LISTSTART (321) 
-//   "<client> Channel :Users  Name"
-// Sent as a reply to the LIST command, this numeric marks the start of a channel list. As noted in the command description, this numeric MAY be skipped by the server so clients MUST NOT depend on receiving it.
-
-// RPL_LIST (322) 
-//   "<client> <channel> <client count> :<topic>"
-// Sent as a reply to the LIST command, this numeric sends information about a channel to the client. 
-//<channel> is the name of the channel. 
-//<client count> is an integer indicating how many clients are joined to that channel. 
-//<topic> is the channel’s topic (as set by the TOPIC command).
-
-// RPL_LISTEND (323) 
-//   "<client> :End of /LIST"
-// Sent as a reply to the LIST command, this numeric indicates the end of a LIST response.
--- srcs/command/Topic.cpp
+++ srcs/command/Topic.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Topic.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkraikua <tkraikua@student.42.th>          +#+  +:+       +#+        */
+/*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 00:32:51 by wluedara          #+#    #+#             */
-/*   Updated: 2024/01/21 20:48:47 by tkraikua         ###   ########.fr       */
+/*   Updated: 2024/01/22 01:15:29 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ Topic::Topic(Server * srv) : Command(srv) {}
 Topic::~Topic() {}
 
 void Topic::execute(Client * client, std::vector<std::string> &args) {
-	std::cout << "[TOPIC] command" << std::endl;
-	// if ( !client->isRegist() ) {
-	// 	client->receive_message(ERR_NOTREGISTERED(_srv->getName()));
-	// 	return;
-	// }
+	if ( !client->isRegist() ) {
+		client->receive_message(ERR_NOTREGISTERED(_srv->getName(), client->getNickname()));
+		return ;
+	}
 	int num = args.size();
 	if (num < 2)
 		client->receive_message(ERR_NEEDMOREPARAMS(_srv->getName(), client->getNickname(), "TOPIC"));
@@ -32,18 +31,20 @@ void Topic::execute(Client * client, std::vector<std::string> &args) {
 			Channel * channel = _srv->getChannel(channel_name.erase(0, 1));
 			if (!channel)
 				client->receive_message(ERR_NOSUCHCHANNEL(_srv->getName(), client->getNickname(), args[1]));
-			else if (client->getChannel(channel_name))
-				client->receive_message(ERR_NOTONCHANNEL(_srv->getName(), client->getNickname(), channel_name));
-			else if (channel->getTopic() != "")
-				client->receive_message(RPL_TOPIC(_srv->getName(), args[1], _srv->getChannel(args[1])->getTopic()));
-			else
-				client->receive_message(RPL_NOTOPIC(_srv->getName(), args[1]));
+			else if (client->getChannel(channel_name)) {
+				if (channel->getTopic() != "")
+					client->receive_message(RPL_TOPIC(_srv->getName(), args[1], _srv->getChannel(args[1])->getTopic()));
+				else
+					client->receive_message(RPL_NOTOPIC(_srv->getName(), args[1]));
+			}
+				// client->receive_message(ERR_NOTONCHANNEL(_srv->getName(), client->getNickname(), channel_name));
 		}
 		else
 			client->receive_message(ERR_UNKNOWNCOMMAND(_srv->getName(), client->getNickname(), "TOPIC"));
 	}
-	else if (num == 3) {
+	else if (num >= 3) {
 			// TOPIC #test : 	Clearing the topic on "#test"
+			client->receive_message(args[1]);
 			if (args[1][0] == ':')
 				_srv->getChannel(args[1])->_setTopic("");
 			else if (args[1][0] == '#') {
@@ -56,17 +57,14 @@ void Topic::execute(Client * client, std::vector<std::string> &args) {
 					client->receive_message(ERR_NOTONCHANNEL(_srv->getName(), client->getNickname(), channel_name));
 				else if (channel->isTopicMode() && !channel->isChanOp(client->getNickname()))
 					client->receive_message(ERR_CHANOPRIVSNEEDED(_srv->getName(), client->getNickname(), channel_name));
-				else
+				else {
+					std::string topic;
+					for (int i = 2; i < num; i++) {
+						topic += args[i];
+					}
+					client->receive_message(args[2]);
 					channel->_setTopic(&args[2][0]);
-				
-				// std::map<std::string, Channel *>::iterator it = _srv->getChannels().begin();
-				// while (it != _srv->getChannels().end()) {
-				// 	if (it->second->getName() == args[1]) {
-				// 		it->second->_setTopic(&args[2][1]);
-				// 		break;
-				// 	}
-				// 	it++;
-				// }
+				}
 			}
 			else
 				client->receive_message(ERR_UNKNOWNCOMMAND(_srv->getName(), client->getNickname(), "TOPIC"));
@@ -74,11 +72,3 @@ void Topic::execute(Client * client, std::vector<std::string> &args) {
 	else
 		client->receive_message(ERR_TOOMANYARGUMENTS(_srv->getName(), "TOPIC"));
 }
-
-// ERR_NEEDMOREPARAMS (461)
-// ERR_NOSUCHCHANNEL (403)
-// ERR_NOTONCHANNEL (442)
-// ERR_CHANOPRIVSNEEDED (482)
-// RPL_NOTOPIC (331)
-// RPL_TOPIC (332)
-// RPL_TOPICWHOTIME (333)
