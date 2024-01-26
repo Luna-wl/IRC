96ee84b2638faf7e92b858df2269f7d21142e55a
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2024-01-26 17:35:31 +0700

    change ascii art

--- srcs/Welcome.cpp
+++ srcs/Welcome.cpp
@@ -1,31 +1,14 @@
 #include "../includes/Server.hpp"
 
 void Server::welcomeMessage(Client * client) {
-	client->recieveMessage(".-=~=-.                                                                  .-=~=-.");
-	client->recieveMessage("(__  _)-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.--(__  _)");
-	client->recieveMessage("(_ ___)                                                                  (_ ___)");
-	client->recieveMessage("( _ __)     █     █░▓█████  ██▓    ▄████▄  ▒█████   ███▄ ▄███▓▓█████     ( _ __)");
-	client->recieveMessage("(__  _)    ▓█░ █ ░█░▓█   ▀ ▓██▒   ▒██▀ ▀█ ▒██▒  ██▒▓██▒▀█▀ ██▒▓█   ▀     (__  _)");
-	client->recieveMessage("(_ ___)    ▒█░ █ ░█ ▒███   ▒██░   ▒▓█    ▄▒██░  ██▒▓██    ▓██░▒███       (_ ___)");
-	client->recieveMessage("(__  _)    ░█░ █ ░█ ▒▓█  ▄ ▒██░   ▒▓▓▄ ▄██▒██   ██░▒██    ▒██ ▒▓█  ▄     (__  _)");
-	client->recieveMessage("(_ ___)    ░░██▒██▓ ░▒████▒░██████▒ ▓███▀ ░ ████▓▒░▒██▒   ░██▒░▒████▒    (_ ___)");
-	client->recieveMessage("(__  _)    ░ ▓░▒ ▒  ░░ ▒░ ░░ ▒░▓  ░ ░▒ ▒  ░ ▒░▒░▒░ ░ ▒░   ░  ░░░ ▒░ ░    (__  _)");
-	client->recieveMessage("(_ ___)      ▒ ░ ░   ░ ░  ░░ ░ ▒  ░ ░  ▒    ░ ▒ ▒░ ░  ░      ░ ░ ░  ░    (_ ___)");
-	client->recieveMessage("(__  _)      ░   ░     ░     ░ ░  ░       ░ ░ ░ ▒  ░      ░      ░       (__  _)");
-	client->recieveMessage("(_ ___)        ░       ░  ░    ░  ░ ░         ░ ░         ░      ░  ░    (_ ___)");
-	client->recieveMessage("(__  _)                           ░                                      (__  _)");
-	client->recieveMessage("( _ __)           ▄▄▄█████▓ ▒█████      ██▓ ██▀███  ▄████▄               ( _ __)");
-	client->recieveMessage("(__  _)           ▓  ██▒ ▓▒▒██▒  ██▒   ▓██▒▓██ ▒ ██▒██▀ ▀█               (_ ___)");
-	client->recieveMessage("(__  _)           ▒ ▓██░ ▒░▒██░  ██▒   ▒██▒▓██ ░▄█ ▒▓█    ▄              (__  _)");
-	client->recieveMessage("(_ ___)           ░ ▓██▓ ░ ▒██   ██░   ░██░▒██▀▀█▄ ▒▓▓▄ ▄██▒             (_ ___)");
-	client->recieveMessage("(__  _)             ▒██▒ ░ ░ ████▓▒░   ░██░░██▓ ▒██▒ ▓███▀ ░             (__  _)");
-	client->recieveMessage("( _ __)             ▒ ░░   ░ ▒░▒░▒░    ░▓  ░ ▒▓ ░▒▓░ ░▒ ▒  ░             ( _ __)");
-	client->recieveMessage("(__  _)               ░      ░ ▒ ▒░     ▒ ░  ░▒ ░ ▒░ ░  ▒                (_ ___)");
-	client->recieveMessage("(_ ___)             ░      ░ ░ ░ ▒      ▒ ░  ░░   ░░                     (__  _)");
-	client->recieveMessage("(__  _)                        ░ ░      ░     ░    ░ ░                   (_ ___)");
-	client->recieveMessage("(_ ___)                                            ░                     (__  _)");
-	client->recieveMessage("(_ ___)-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.--(_ ___)");
-	client->recieveMessage("`-._.-'                                                                  `-._.-'");
+	client->recieveMessage("██████╗ ██╗   ██╗██████╗  ██████╗ ██╗     ██████╗ ██╗  ██╗");
+	client->recieveMessage("██╔══██╗██║   ██║██╔══██╗██╔═══██╗██║     ██╔══██╗██║  ██║");
+	client->recieveMessage("██████╔╝██║   ██║██║  ██║██║   ██║██║     ██████╔╝███████║");
+	client->recieveMessage("██╔══██╗██║   ██║██║  ██║██║   ██║██║     ██╔═══╝ ██╔══██║");
+	client->recieveMessage("██║  ██║╚██████╔╝██████╔╝╚██████╔╝███████╗██║     ██║  ██║");
+	client->recieveMessage("╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝╚═╝     ╚═╝  ╚═╝");
+	client->recieveMessage("                            welcome to RUDOLPH irc, Enjoy!");
+	client->recieveMessage("                                      " + time(1));
 }
 
 
