bee79af7ec6dfa620b7b1c06f5752f64f04a19dd
Luna-wl <Warintorn_L@outlook.com>
2024-01-28 22:48:13 +1100

    Update Time command execution in Time.cpp

--- srcs/command/Time.cpp
+++ srcs/command/Time.cpp
@@ -6,7 +6,7 @@
 /*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 11:40:45 by csantivimol       #+#    #+#             */
-/*   Updated: 2024/01/28 22:05:28 by wluedara         ###   ########.fr       */
+/*   Updated: 2024/01/28 22:47:40 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ Time::Time(Server * srv) : Command(srv) {}
 Time::~Time() {}
 
 void Time::execute(Client * client, std::vector<std::string> & args) {
-    client->receive_message(RPL_TIME(client->source(), args[0], _srv->getName(), _srv->time(0), _srv->time(2)));
+	client->receive_message(RPL_TIME(client->source(), args[0], _srv->getName(), _srv->time(0), _srv->time(2)));
 }
