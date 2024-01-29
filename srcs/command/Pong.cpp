aa44bc3171081d6648d319609d2b32efdbd789ad
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2024-01-29 15:50:24 +0700

    fix ping pong

--- srcs/command/Pong.cpp
+++ srcs/command/Pong.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Pong.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkraikua <tkraikua@student.42.th>          +#+  +:+       +#+        */
+/*   By: csantivimol <csantivimol@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 15:53:29 by csantivimol       #+#    #+#             */
-/*   Updated: 2024/01/28 19:15:47 by tkraikua         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:49:54 by csantivimol      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ Pong::Pong(Server * srv) : Command(srv) {}
 Pong::~Pong() {}
 
 void Pong::execute(Client * client, std::vector<std::string> & args) {
-  if (args.size() > 2)
+  if (args.size() > 3)
 		client->receiveMessage(ERR_TOOMANYARGUMENTS(_srv->getName(), args[0]));
-	else if (args[1].empty())
+	else if (args.size() < 2)
 		client->receiveMessage(ERR_NEEDMOREPARAMS(_srv->getName(), client->getNickname(), args[0]));
 	else
 		client->receiveMessage(RPL_PONG(client->source(), args[1]));
